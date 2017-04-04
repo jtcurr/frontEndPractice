c2f3aae0f323cac33d5e11f50a646cc184b6c4b8
John Currie <johntcurrie@gmail.com>
2017-04-04 03:21:23 -0700

    Added event listener to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-let logo = document.getElementById('logo');
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  let name = data.name.split(' ');
+  let firstLetters = '';
+  for (let i = 0; i <= name.length-1; i++) {
+    firstLetters += name[i][0];
+  }
+  document.getElementById('logo').innerHTML = firstLetters;
+  
+});
