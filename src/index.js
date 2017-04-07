71a2d11a918db02e0b987ba122102edf7e1446e6
John Currie <johntcurrie@gmail.com>
2017-04-07 15:51:14 -0700

    Added follow button click listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
   let name = data.name.split(' ');
   let firstLetters = '';
   for (let i = 0; i <= name.length-1; i++) {
@@ -8,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('outerLogo').innerHTML += firstLetters;
   document.getElementById('companyTitle').innerHTML += data.name;
   document.getElementById('slogan').innerHTML += data.slogan + ' - ';
-  var dataTag;
-  var result;
-  for (var x = 0; x < data.tags.length; x++ ) {
+  document.getElementById('city').innerHTML += data.city;
+  document.getElementById('join').innerHTML += 'Joined ' + data.joinDate;
+  document.getElementById('site').innerHTML += data.shortWebsite.link(data.fullWebsite);
+  
+	for (var x = 0; x < data.tags.length; x++ ) {
     if (x === data.tags.length-1) {
     	document.getElementById('tags').innerHTML += ', and ' + '#' + data.tags[x].link(data.tagURLS[data.tags[x]]);
 
@@ -19,5 +20,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     } else {
     	document.getElementById('tags').innerHTML += ', ' + '#' + data.tags[x].link(data.tagURLS[data.tags[x]]);
     }
+  }
+
+  document.getElementById('follow').addEventListener('click', function() {buttonFunction()});
+
+  function buttonFunction (e) {
+    
   }
 });
