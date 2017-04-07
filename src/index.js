9439470d7a55324c0b44c41a12bc272a26163677
John Currie <johntcurrie@gmail.com>
2017-04-07 14:31:54 -0700

    Added slogan from dummy data

--- src/index.js
+++ src/index.js
@@ -8,4 +8,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('outerLogo').innerHTML += firstLetters;
   document.getElementById('companyTitle').innerHTML += data.name;
   document.getElementById('slogan').innerHTML += data.slogan + ' - ';
+  for (var x = 0; x < data.tags.length-1; x++ ) {
+    if (x === data.tags.length-2) {
+    	document.getElementById('tags') += ', and ' + data.tags[x];
+    } if (x === 0) {
+      document.getElementById('tags') += data.tags[x];
+    } else {
+    	document.getElementById('tags') += data.tags[x];
+    }
+  }
 });
