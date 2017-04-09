10ef9e654973debd02c3890348d3ed496218d6dd
John Currie <johntcurrie@gmail.com>
2017-04-08 18:51:57 -0700

    Added SRC element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
+  //let elem = document.createElement('src');
+  let elem = document.createElement('img');
+  elem.setAttribute('src', data.background);
+  document.getElementById('bannerImage').appendChild(elem);
+  
   let name = data.name.split(' ');
   let firstLetters = '';
   for (let i = 0; i <= name.length-1; i++) {
